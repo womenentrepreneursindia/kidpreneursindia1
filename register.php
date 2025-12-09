d2bb00b3146eeb480db7b04add88d4d71ceea733
womenentrepreneursindia <248684071+womenentrepreneursindia@users.noreply.github.com>
2025-12-09 13:33:05 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ register.php
@@ -0,0 +1,512 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="" />
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i" rel="stylesheet">
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="css/responsive.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!-- Document Title
+	============================================= -->
+	<title>Kidpreneurs India  | Register</title>
+
+</head>
+
+<body class="stretched side-push-panel">
+	<div class="body-overlay"></div>
+	<div id="side-panel" class="dark">
+
+		<div id="side-panel-trigger-close" class="side-panel-trigger"><a href="#"><i class="icon-line-cross"></i></a></div>
+
+		<div class="side-panel-wrap">
+
+			<div class="widget widget_links clearfix">
+
+				<h4 style="font-weight:300;line-height: 1.7;font-family:'Roboto', sans-serif;">Kidpreneurs India</h4>
+
+				<div style="font-size: 14px; font-weight:200;line-height: 1.7;font-family:'Roboto', sans-serif;">
+					<address style="line-height: 1.7;">
+						
+						Velachery main road,<br>
+						Guindy,Chennai-600 032<br>
+						Tamil Nadu , India<br>
+					</address>
+
+					<div class="clear topmargin-sm"></div>
+
+					<abbr title="Phone Number">Phone:</abbr> (+91) 044- 42031721 <br>
+					<abbr title="Mobile Number">Mobile:</abbr> (+91) 99520 13800 <br>
+					<!-- <abbr title="Fax">Fax:</abbr> (91) 11 4752 1433<br> -->
+					<abbr title="Email Address">Email:</abbr>kidpreneursindia@gmail.com
+				</div>
+
+			</div>
+
+			<div class="widget quick-contact-widget clearfix">
+
+				<h4 style="font-weight:300;line-height: 1.7;font-family:'Roboto', sans-serif;">Connect Socially</h4>
+
+				<a href="#" class="social-icon si-small si-colored si-facebook" title="Facebook">
+					<i class="icon-facebook"></i>
+					<i class="icon-facebook"></i>
+				</a>
+				<!-- <a href="#" class="social-icon si-small si-colored si-twitter" title="Twitter">
+					<i class="icon-twitter"></i>
+					<i class="icon-twitter"></i>
+				</a>
+				<a href="#" class="social-icon si-small si-colored si-github" title="Github">
+					<i class="icon-github"></i>
+					<i class="icon-github"></i>
+				</a>
+				<a href="#" class="social-icon si-small si-colored si-pinterest" title="Pinterest">
+					<i class="icon-pinterest"></i>
+					<i class="icon-pinterest"></i>
+				</a>
+				<a href="#" class="social-icon si-small si-colored si-forrst" title="Forrst">
+					<i class="icon-forrst"></i>
+					<i class="icon-forrst"></i>
+				</a>
+				<a href="#" class="social-icon si-small si-colored si-linkedin" title="LinkedIn">
+					<i class="icon-linkedin"></i>
+					<i class="icon-linkedin"></i>
+				</a>
+				<a href="#" class="social-icon si-small si-colored si-gplus" title="Google Plus">
+					<i class="icon-gplus"></i>
+					<i class="icon-gplus"></i>
+				</a>
+				<a href="#" class="social-icon si-small si-colored si-instagram" title="Instagram">
+					<i class="icon-instagram"></i>
+					<i class="icon-instagram"></i>
+				</a>
+				<a href="#" class="social-icon si-small si-colored si-flickr" title="Flickr">
+					<i class="icon-flickr"></i>
+					<i class="icon-flickr"></i>
+				</a>
+				<a href="#" class="social-icon si-small si-colored si-skype" title="Skype">
+					<i class="icon-skype"></i>
+					<i class="icon-skype"></i>
+				</a> -->
+
+			</div>
+
+		</div>
+
+	</div>
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+		<!-- Top Bar
+		============================================= -->
+		<!--<div id="top-bar">
+
+			<div class="container clearfix">
+
+				<div class="col_half nobottommargin clearfix">
+
+					
+					<div class="top-links">
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="faqs.html">FAQs</a></li>
+							<li><a href="contact.html">Contact</a></li>
+							<li><a href="login-register.html">Login</a>
+								<div class="top-link-section">
+									<form id="top-login" role="form">
+										<div class="input-group" id="top-login-username">
+											<span class="input-group-addon"><i class="icon-user"></i></span>
+											<input type="email" class="form-control" placeholder="Email address" required="">
+										</div>
+										<div class="input-group" id="top-login-password">
+											<span class="input-group-addon"><i class="icon-key"></i></span>
+											<input type="password" class="form-control" placeholder="Password" required="">
+										</div>
+										<label class="checkbox">
+										  <input type="checkbox" value="remember-me"> Remember me
+										</label>
+										<button class="btn btn-danger btn-block" type="submit">Sign in</button>
+									</form>
+								</div>
+							</li>
+						</ul>
+					</div>
+
+				</div>
+
+				<div class="col_half fright col_last clearfix nobottommargin">
+
+					
+					<div id="top-social">
+						<ul>
+							<li><a href="#" class="si-facebook"><span class="ts-icon"><i class="icon-facebook"></i></span><span class="ts-text">Facebook</span></a></li>
+							<li><a href="#" class="si-twitter"><span class="ts-icon"><i class="icon-twitter"></i></span><span class="ts-text">Twitter</span></a></li>
+							<li><a href="#" class="si-dribbble"><span class="ts-icon"><i class="icon-dribbble"></i></span><span class="ts-text">Dribbble</span></a></li>
+							<li><a href="#" class="si-github"><span class="ts-icon"><i class="icon-github-circled"></i></span><span class="ts-text">Github</span></a></li>
+							<li><a href="#" class="si-pinterest"><span class="ts-icon"><i class="icon-pinterest"></i></span><span class="ts-text">Pinterest</span></a></li>
+							<li><a href="#" class="si-instagram"><span class="ts-icon"><i class="icon-instagram2"></i></span><span class="ts-text">Instagram</span></a></li>
+							<li><a href="tel:+91.11.85412542" class="si-call"><span class="ts-icon"><i class="icon-call"></i></span><span class="ts-text">+91.11.85412542</span></a></li>
+							<li><a href="mailto:info@canvas.com" class="si-email3"><span class="ts-icon"><i class="icon-envelope-alt"></i></span><span class="ts-text">info@canvas.com</span></a></li>
+						</ul>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>-->
+		<!-- #top-bar end -->
+
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header transparent-header border-full-header dark static-sticky" data-sticky-class="not-dark" data-sticky-offset="full" data-sticky-offset-negative="100" style="background-color:#3a3434;">
+
+			<div id="header-wrap">
+
+				<div class="container clearfix">
+
+					<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+					<!-- Logo
+					============================================= -->
+					<div id="logo">
+						<a href="index.html" class="standard-logo" data-dark-logo="images/logo2x.png"><img src="images/logo.png" alt="Kidpreneurs India Logo"></a>
+						<a href="index.html" class="retina-logo" data-dark-logo="images/logo2x.png"><img src="images/logo2x.png" alt="Kidpreneurs India Logo"></a>
+					</div><!-- #logo end -->
+
+					<!-- Primary Navigation
+					============================================= -->
+					<nav id="primary-menu">
+
+						<ul class="one-page-menu" data-easing="easeInOutExpo" data-speed="1250" data-offset="65">
+							<li><a href="index.html" data-href="#wrapper"><div>Home</div></a></li>
+							<li><a href="index.html" data-href="#section-about"><div>About</div></a></li>
+							<li><a href="index.html" data-href="#section-works"><div>Work Shops</div></a></li>
+							<li><a href="index.html" data-href="#section-services"><div>Hobby Classes</div></a></li>
+							<li><a href="events.html" data-href="#"><div>Events</div></a></li>
+							<!-- <li><a href="#" data-href="#section-blog"><div>Blog</div></a></li>  -->
+							<li><a href="index.html" data-href="#section-contact"><div>Contact</div></a></li>
+						</ul>
+
+						<div id="side-panel-trigger" class="side-panel-trigger"><a href="#"><i class="icon-reorder"></i></a></div>
+
+					</nav><!-- #primary-menu end -->
+
+				</div>
+
+			</div>
+
+		</header><!-- #header en
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title">
+
+			<div class="container clearfix">
+				<h1>My Account</h1>
+				<ol class="breadcrumb">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="events.html">Events</a></li>
+					<li class="active">Register</li>
+				</ol>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+
+			<div class="content-wrap">
+
+				<div class="container clearfix">
+
+					<!-- <div class="col_one_third nobottommargin">
+
+						<div class="well well-lg nobottommargin">
+							<form id="login-form" name="login-form" class="nobottommargin" action="#" method="post">
+
+								<h3>Login to your Account</h3>
+
+								<div class="col_full">
+									<label for="login-form-username">Username:</label>
+									<input type="text" id="login-form-username" name="login-form-username" value="" class="form-control" />
+								</div>
+
+								<div class="col_full">
+									<label for="login-form-password">Password:</label>
+									<input type="password" id="login-form-password" name="login-form-password" value="" class="form-control" />
+								</div>
+
+								<div class="col_full nobottommargin">
+									<button class="button button-3d nomargin" id="login-form-submit" name="login-form-submit" value="login">Login</button>
+									<a href="#" class="fright">Forgot Password?</a>
+								</div>
+
+							</form>
+						</div>
+
+					</div> -->
+
+					<div class="col_two_third col_last nobottommargin">
+
+
+						<h3>Don't have an Account? Register Now.</h3>
+
+						<p>Register to become an active member in our community.</p>
+						
+						<form id="register-form" name="register-form" class="nobottommargin" action="action.php" method="post">
+
+							<div class="col_half">
+								<label for="register-form-name">Name:</label>
+								<input type="text" id="register-form-name" name="txtuname" value="" class="form-control" placeholder="Enter Username" required />
+							</div>
+<div class="clear"></div>
+							<div class="col_half col_last">
+								<label for="register-form-email">Email Address:</label>
+								<input type="email" id="register-form-email" name="txtemail" value="" class="form-control" placeholder="Enter Email" required />
+							</div>
+
+							
+
+							<div class="clear"></div>
+
+							<div class="col_half">
+								<label for="register-form-password">Choose Password:</label>
+								<input type="password" id="register-form-password" name="txtpass" value="" class="form-control" placeholder="Enter Password" required />
+							</div>
+
+							<!-- <div class="col_half col_last">
+								<label for="register-form-repassword">Re-enter Password:</label>
+								<input type="password" id="register-form-repassword" name="txt_repass" value="" class="form-control" placeholder="Re-enter Password"required />
+							</div>
+							
+							<div class="clear"></div> -->
+
+							<!--  <div class="col_half">
+								<label for="register-form-username">Choose a Username:</label>
+								<input type="text" id="register-form-username" name="register-form-username" value="" class="form-control" />
+							</div> --> 
+
+							<!-- <div class="col_half col_last">
+								<label for="register-form-phone">Phone:</label>
+								<input type="text" id="register-form-phone" name="txtphone" value="" class="form-control" placeholder="Enter Phone/Mobile Number"required />
+							</div> -->
+
+							<div class="clear"></div>
+                                <?php if(isset($msg)) echo $msg;  ?>
+                            <div class="clear"></div>
+							<div class="col_full nobottommargin">
+								<button class="button button-3d button-black nomargin" id="register-form-submit" type="submit" name="btn-signup" value="register">Register Now</button>
+							</div>
+
+						</form>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<footer id="footer" class="dark noborder">
+
+			<!-- <div class="container">
+
+				
+				<div class="footer-widgets-wrap clearfix">
+
+					<div class="col_two_third">
+
+						<div class="col_one_third">
+
+							<div class="widget clearfix">
+
+								<img src="images/footer-widget-logo.png" alt="" class="footer-logo">
+
+								<p>We believe in <strong>Simple</strong>, <strong>Creative</strong> &amp; <strong>Flexible</strong> Design Standards.</p>
+
+								<div style="background: url('images/world-map.png') no-repeat center center; background-size: 100%;">
+									<address>
+										<strong>Headquarters:</strong><br>
+										795 Folsom Ave, Suite 600<br>
+										San Francisco, CA 94107<br>
+									</address>
+									<abbr title="Phone Number"><strong>Phone:</strong></abbr> (91) 8547 632521<br>
+									<abbr title="Fax"><strong>Fax:</strong></abbr> (91) 11 4752 1433<br>
+									<abbr title="Email Address"><strong>Email:</strong></abbr> info@canvas.com
+								</div>
+
+							</div>
+
+						</div>
+
+						<div class="col_one_third">
+
+							<div class="widget widget_links clearfix">
+
+								<h4>Blogroll</h4>
+
+								<ul>
+									<li><a href="http://codex.wordpress.org/">Documentation</a></li>
+									<li><a href="http://wordpress.org/support/forum/requests-and-feedback">Feedback</a></li>
+									<li><a href="http://wordpress.org/extend/plugins/">Plugins</a></li>
+									<li><a href="http://wordpress.org/support/">Support Forums</a></li>
+									<li><a href="http://wordpress.org/extend/themes/">Themes</a></li>
+									<li><a href="http://wordpress.org/news/">WordPress Blog</a></li>
+									<li><a href="http://planet.wordpress.org/">WordPress Planet</a></li>
+								</ul>
+
+							</div>
+
+						</div>
+
+						<div class="col_one_third col_last">
+
+							<div class="widget clearfix">
+								<h4>Recent Posts</h4>
+
+								<div id="post-list-footer">
+									<div class="spost clearfix">
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Lorem ipsum dolor sit amet, consectetur</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li>10th July 2014</li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="spost clearfix">
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Elit Assumenda vel amet dolorum quasi</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li>10th July 2014</li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="spost clearfix">
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Debitis nihil placeat, illum est nisi</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li>10th July 2014</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="col_one_third col_last">
+
+						<div class="widget clearfix" style="margin-bottom: -20px;">
+
+							<div class="row">
+
+								<div class="col-md-6 bottommargin-sm">
+									<div class="counter counter-small"><span data-from="50" data-to="15065421" data-refresh-interval="80" data-speed="3000" data-comma="true"></span></div>
+									<h5 class="nobottommargin">Total Downloads</h5>
+								</div>
+
+								<div class="col-md-6 bottommargin-sm">
+									<div class="counter counter-small"><span data-from="100" data-to="18465" data-refresh-interval="50" data-speed="2000" data-comma="true"></span></div>
+									<h5 class="nobottommargin">Clients</h5>
+								</div>
+
+							</div>
+
+						</div>
+
+						<div class="widget subscribe-widget clearfix">
+							<h5><strong>Subscribe</strong> to Our Newsletter to get Important News, Amazing Offers &amp; Inside Scoops:</h5>
+							<div class="widget-subscribe-form-result"></div>
+							<form id="widget-subscribe-form" action="include/subscribe.php" role="form" method="post" class="nobottommargin">
+								<div class="input-group divcenter">
+									<span class="input-group-addon"><i class="icon-email2"></i></span>
+									<input type="email" id="widget-subscribe-form-email" name="widget-subscribe-form-email" class="form-control required email" placeholder="Enter your Email">
+									<span class="input-group-btn">
+										<button class="btn btn-success" type="submit">Subscribe</button>
+									</span>
+								</div>
+							</form>
+						</div>
+
+						<div class="widget clearfix" style="margin-bottom: -20px;">
+
+							<div class="row">
+
+								<div class="col-md-6 clearfix bottommargin-sm">
+									<a href="#" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
+										<i class="icon-facebook"></i>
+										<i class="icon-facebook"></i>
+									</a>
+									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Like us</strong><br>on Facebook</small></a>
+								</div>
+								<div class="col-md-6 clearfix">
+									<a href="#" class="social-icon si-dark si-colored si-rss nobottommargin" style="margin-right: 10px;">
+										<i class="icon-rss"></i>
+										<i class="icon-rss"></i>
+									</a>
+									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Subscribe</strong><br>to RSS Feeds</small></a>
+								</div>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+ -->
+			<!-- Copyrights
+			============================================= -->
+			<div id="copyrights">
+				<div class="container center clearfix" style="font-weight:200;">
+					Copyright © Kidpreneurs India 2021 | All Rights Reserved
+				</div>
+			</div>
+
+		</footer><!-- #footer end -->
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+</body>
+</html>
